f968de24997bdba819503dd51c915498d18283c6
Omar <omar@example.com>
2025-03-09 18:36:20 +0200

    Configuring the  Database New migrations and changing the database to MySql

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,37 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = [
-            [
-                'id' => 1, 
-                'title' => 'laravel', 
-                'description' => 'Laravel is a powerful PHP framework for web development',
-                'posted_by' => 'ahmed', 
-                'created_at' => '2025-03-08 12:47:00'
-            ],
-            [
-                'id' => 2, 
-                'title' => 'HTML', 
-                'description' => 'HTML is the standard markup language for creating web pages',
-                'posted_by' => 'mohamed', 
-                'created_at' => '2025-04-10 11:00:00'
-            ],
-            [
-                'id' => 3, 
-                'title' => 'CSS', 
-                'description' => 'CSS is the language used to style web pages',
-                'posted_by' => 'sara', 
-                'created_at' => '2025-04-15 09:30:00'
-            ],
-        ];
-
+      $posts = Post::all();
+      dd($posts);
         return view('posts.index', ['posts' => $posts]);
     }
 
