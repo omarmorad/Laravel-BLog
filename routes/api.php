c7a0137111226747610af69bc2ddaa3a6631b228
Omar <omar@example.com>
2025-03-16 19:30:29 +0200

    Added Auth Tokens for the APi

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,31 @@
 
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
+use \App\Models\User;
+use \Illuminate\Validation\ValidationException;
+use \Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('posts', [PostController::class,'index']);
+Route::middleware('auth:sanctum')->get('posts', [PostController::class,'index']);
 Route::get('posts/{post}', [PostController::class,'show']);
 Route::post('posts' , [PostController::class,'store']);
+
+Route::post('/sanctum/token', function(Request $request){
+    $request->validate([
+        'email' => 'required|email',
+        'password'=> 'required',
+        'device_name'=>'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    return $user->createToken($request->device_name)->plainTextToken;
+});
